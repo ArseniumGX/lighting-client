a268cfca5a5e6effdb4a3573feb263b4408a3843
José Pereira Macedo <arseniumgx@gmail.com>
2022-01-04 21:19:53 -0300

    Page Signup

---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,99 @@
+import { useState } from 'react'
+import { Input, Button } from '../../components'
+import axios from 'axios'
+import style from './Signup.module.scss'
+
+export function Signup() {
+  const [user, setUser] = useState({})
+
+  const handleFields = (e) => {
+    const aux = { ...user }
+    aux[e.target.name] = e.target.value
+    setUser(aux)
+  }
+
+  const sendUser = async (e) => {
+    e.preventDefault()
+
+    await axios
+      .post('user', user)
+      .then((res) => console.log(res))
+      .catch((error) => {
+        if (error.response.status === 406) alert(error.response.data.message)
+        else if (error.response.status === 409)
+          alert(error.response.data.message)
+        else console.error(error.message)
+      })
+  }
+
+  return (
+    <section className={style.signup}>
+      <form onSubmit={sendUser}>
+        <Input
+          title="Nome Completo"
+          id="fullname"
+          type="text"
+          name="fullname"
+          value={user.fullname || ''}
+          onChange={handleFields}
+          required={false}
+        />
+        <Input
+          title="Data de Nascimento"
+          id="birthdate"
+          type="date"
+          name="birthdate"
+          value={user.birthdate || ''}
+          onChange={handleFields}
+          required={false}
+        />
+        <Input
+          title="Nickname"
+          id="nickname"
+          type="text"
+          name="nickname"
+          value={user.nickname || ''}
+          onChange={handleFields}
+          required={true}
+        />
+        <Input
+          title="URL Imagem"
+          id="imageUrl"
+          type="url"
+          name="imageUrl"
+          value={user.imageUrl || ''}
+          onChange={handleFields}
+          required={false}
+        />
+        <Input
+          title="Email"
+          id="email"
+          type="email"
+          name="email"
+          value={user.email || ''}
+          onChange={handleFields}
+          required={true}
+        />
+        <Input
+          title="Senha"
+          id="password"
+          type="password"
+          name="password"
+          value={user.password || ''}
+          onChange={handleFields}
+          required={true}
+        />
+        <Input
+          title="Confirmação de Senha"
+          id="passwordConfirm"
+          type="password"
+          name="passwordConfirm"
+          value={user.passwordConfirm || ''}
+          onChange={handleFields}
+          required={true}
+        />
+        <Button value="Cadastrar" />
+      </form>
+    </section>
+  )
+}
