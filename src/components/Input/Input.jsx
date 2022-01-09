3f6f4e45518e9944f9d3a6c3581122bfb57ddc41
José Pereira Macedo <arseniumgx@gmail.com>
2022-01-09 19:37:55 -0300

    Ajustment on definition

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,17 +1,20 @@
+import { Fragment } from 'react'
 import style from './Input.module.scss'
 
 export function Input(props) {
   return (
-    <>
+    <Fragment>
       <label htmlFor={props.id}>{props.label}</label>
       <input
         className={style.input}
+        id={props.id}
         type={props.type}
         value={props.value}
         name={props.name}
         placeholder={props.placeholder}
         onChange={props.onChange}
+        disabled={props.disabled}
       />
-    </>
+    </Fragment>
   )
 }
