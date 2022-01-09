9f340eeb680293b022bf448fa53876d8dbdf46cd
José Pereira Macedo <arseniumgx@gmail.com>
2022-01-09 19:35:13 -0300

    Cleanup code

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,15 @@
 import style from './Button.module.scss'
 
 function Button(props) {
-   return (
-      <>
-         <div className={style.button}>
-            <button onClick={props.onClick}>{props.value}</button>
-         </div>
-      </>
-   )
+  return (
+    <>
+      <div className={style.button}>
+        <button onClick={props.onClick} type={props.type}>
+          {props.value}
+        </button>
+      </div>
+    </>
+  )
 }
 
 export { Button }
