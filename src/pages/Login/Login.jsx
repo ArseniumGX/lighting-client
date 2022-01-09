ea849f0f10f1e113e961f995a57f9e931d9ecdbe
José Pereira Macedo <arseniumgx@gmail.com>
2022-01-09 16:48:33 -0300

    Login interface is done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,41 +1,81 @@
-import {} from 'react'
-import {} from 'react-router-dom'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { FaEdit, FaUser } from 'react-icons/fa'
 import style from './Login.module.scss'
-import { Input } from './Input/Input'
+import { Button, Input } from '../../components'
 
 export function Login() {
+  const [user, setUser] = useState({})
+  const [cademail, setCademail] = useState('')
+  const navigate = useNavigate()
+
+  const handleUser = (e) => {
+    const aux = { ...user }
+
+    aux[e.target.name] = e.target.value
+
+    setUser(aux)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(JSON.stringify(user))
+  }
+
+  const makeAccount = (e) => {
+    if (cademail) {
+      navigate('/register', { state: { email: cademail } })
+    }
+  }
+
   return (
     <section className={style.login}>
       <span className={style.login__header}>
         Faça seu login ou crie uma conta caso ainda não seja cadastrado
       </span>
+
       <div className={style.login__frame}>
         <div className={style.login__frame__signin}>
           <h3>
             <FaUser /> Já é cadastrado ?
           </h3>
           <hr />
-          <Input type="email" value={null} name="email" placeholder="E-mail" />
-          <Input
-            type="password"
-            value={null}
-            name="password"
-            placeholder="Senha"
-          />
+          <form onSubmit={handleSubmit}>
+            <Input
+              type="email"
+              value={user.email}
+              name="email"
+              placeholder="E-mail"
+              onChange={handleUser}
+            />
+            <Input
+              type="password"
+              value={user.passpord}
+              name="password"
+              placeholder="Senha"
+              onChange={handleUser}
+            />
+            <Button value="Prosseguir" />
+          </form>
         </div>
+
         <div className={style.login__frame__signup}>
           <h3>
             <FaEdit /> Ainda não possui uma conta ?
           </h3>
           <hr />
           <span>Digite seu email para continuar</span>
-          <Input
-            type="email"
-            value={null}
-            name="cademail"
-            placeholder="E-mail"
-          />
+          <div>
+            <Input
+              type="email"
+              value={cademail}
+              name="cademail"
+              placeholder="E-mail"
+              onChange={(e) => setCademail(e.target.value)}
+            />
+          </div>
+          <Button onClick={makeAccount} value="Cadastrar" />
         </div>
       </div>
     </section>
