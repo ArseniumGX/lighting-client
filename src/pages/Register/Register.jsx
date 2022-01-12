3c537fbc5381a9a7b44fa618cdda0e6688a96936
José Pereira Macedo <arseniumgx@gmail.com>
2022-01-11 23:09:07 -0300

    Register form mounted

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { HiMail, HiUserCircle, HiKey, HiIdentification } from 'react-icons/hi'
 import style from './Register.module.scss'
-import { Input, Button } from '../../components'
+import { Button, InputLeftLabel, InputUpperLabel } from '../../components'
 
 export function Register() {
   const [user, setUser] = useState({})
   const location = useLocation()
+  const navigator = useNavigate()
 
   const handleFields = (e) => {
     const aux = { ...user }
@@ -17,6 +18,15 @@ export function Register() {
   const sendUser = async (e) => {
     e.preventDefault()
 
+    if (
+      user.email !== user.emailConfirm ||
+      user.password !== user.passwordConfirm
+    ) {
+      alert('Dados não confere')
+      return
+    }
+
+    delete user.emailConfirm
     console.info(user)
   }
 
@@ -27,39 +37,42 @@ export function Register() {
   return (
     <section className={style.register}>
       <form onSubmit={sendUser}>
-        <div className={style.register__access_info}>
+        <div className={style.register__accessInfo}>
           <span>
             <HiUserCircle />
             Dados de acesso
           </span>
-          <div className={style.register__access_info__box}>
-            <Input
+          <div className={style.register__accessInfo__box}>
+            <InputUpperLabel
               id="email"
               type="email"
               value={user.email || ''}
               name="email"
               onChange={handleFields}
+              disabled={true}
               label={
                 <>
                   <HiMail /> Email
                 </>
               }
             />
-            <Input
+            <InputUpperLabel
               id="email-confirm"
               type="email"
               value={user.emailConfirm || ''}
               name="emailConfirm"
               onChange={handleFields}
+              required={true}
               label="Confirme o e-mail"
             />
           </div>
-          <div className={style.register__access_info__box}>
-            <Input
+          <div className={style.register__accessInfo__box}>
+            <InputUpperLabel
               id="password"
               type="password"
               value={user.password || ''}
               name="password"
+              required={true}
               onChange={handleFields}
               label={
                 <>
@@ -67,24 +80,25 @@ export function Register() {
                 </>
               }
             />
-            <Input
+            <InputUpperLabel
               id="passord-confirm"
               type="password"
               value={user.passwordConfirm || ''}
               name="passwordConfirm"
               onChange={handleFields}
               label="Confirma a senha"
+              required={true}
             />
           </div>
         </div>
 
-        <div className={style.register__personal_info}>
+        <div className={style.register__personalInfo}>
           <span>
             <HiIdentification />
             Informações Pessoais
           </span>
           <div>
-            <Input
+            <InputLeftLabel
               id="name"
               type="text"
               value={user.name || ''}
@@ -95,7 +109,7 @@ export function Register() {
             />
           </div>
           <div>
-            <Input
+            <InputLeftLabel
               id="avatar"
               type="text"
               value={user.avatar || ''}
@@ -105,7 +119,7 @@ export function Register() {
             />
           </div>
           <div>
-            <Input
+            <InputLeftLabel
               id="name"
               type="date"
               value={user.birthdate || ''}
@@ -114,29 +128,20 @@ export function Register() {
               label="Data de nascimento"
             />
           </div>
-          <div className={style.regiter__personal_info__gender_box}>
-            <label>Gênero</label>
-            <div>
-              <Input
-                id="m"
-                type="radio"
-                value="Masculino"
-                name="gender"
-                label="Masculino"
-                onChange={handleFields}
-              />
-              <Input
-                id="f"
-                type="radio"
-                value="Feminino"
-                name="gender"
-                label="Feminino"
-                onChange={handleFields}
-              />
-            </div>
+          <div className={style.register__personalInfo__genderBox}>
+            <label htmlFor="gender">Gênero</label>
+            <select
+              name="gender"
+              id="gender"
+              onChange={(e) => setUser({ gender: e.target.value })}
+            >
+              <option value=""></option>
+              <option value="masculino">Masculino</option>
+              <option value="feminino">Feminino</option>
+            </select>
           </div>
           <div>
-            <Input
+            <InputLeftLabel
               id="phone"
               type="telephone"
               value={user.phone || ''}
@@ -148,7 +153,14 @@ export function Register() {
         </div>
         <div className={style.register__buttons}>
           <Button type="submit" value="Cadastrar" />
-          <Button type="click" value="Cancelar" />
+          <Button
+            type="click"
+            value="Cancelar"
+            onClick={(e) => {
+              e.preventDefault()
+              navigator('/login', { replace: true })
+            }}
+          />
         </div>
       </form>
     </section>
