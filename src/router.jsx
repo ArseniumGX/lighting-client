cc93a7d5032559f1b4dfba0dbf362db2964d955b
José Pereira Macedo <arseniumgx@gmail.com>
2022-01-12 23:00:39 -0300

    Define final routes

--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import { Register, Home, Book, Login } from './pages'
+import { Register, Home, Book, Login, About, NotFound, Profile } from './pages'
 
 export function Router() {
   return (
@@ -7,7 +7,10 @@ export function Router() {
       <Route path="/" element={<Home />} />
       <Route path="login" element={<Login />} />
       <Route path="register" element={<Register />} />
-      <Route path="book" element={<Book></Book>} />
+      <Route path="book" element={<Book />} />
+      <Route path="profile" element={<Profile></Profile>} />
+      <Route path="about" element={<About></About>} />
+      <Route path="*" element={<NotFound></NotFound>} />
     </Routes>
   )
 }
